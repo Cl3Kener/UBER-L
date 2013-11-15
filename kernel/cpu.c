ca14c62ec96d467c8e0e16eb051ccc4fd54c6874
Dennis Rassmann <showp1984@gmail.com>
2013-11-15 05:24:06 +0100

    cpu: noop nonboot_cpu calls if MSM_MPDEC is compiled
    Signed-off-by: Dennis Rassmann <showp1984@gmail.com>
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -413,6 +413,7 @@ EXPORT_SYMBOL_GPL(cpu_up);
 #ifdef CONFIG_PM_SLEEP_SMP
 static cpumask_var_t frozen_cpus;
 
+#if !defined(CONFIG_MSM_MPDEC)
 void __weak arch_disable_nonboot_cpus_begin(void)
 {
 }
@@ -498,6 +499,7 @@ void __ref enable_nonboot_cpus(void)
 out:
 	cpu_maps_update_done();
 }
+#endif
 
 static int __init alloc_frozen_cpus(void)
 {
