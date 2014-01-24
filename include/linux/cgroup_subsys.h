2be4371584854c70cc92000373a7a1f0cf6fe5c4
hellsgod <hellsgod@gmx.ch>
2014-01-24 08:16:45 +0100

    cgroups: timer_slack: forward port timer_slack to 3.4. http://lwn.net/Articles/431847/
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>
    Signed-off-by: Chester Kener <Cl3Kener@gmail.com>
    
    Conflicts:
    	include/linux/cgroup_subsys.h

--- include/linux/cgroup_subsys.h
+++ include/linux/cgroup_subsys.h
@@ -78,3 +78,9 @@ SUBSYS(bfqio)
 #endif
 
 /* */
+
+#ifdef CONFIG_CGROUP_TIMER_SLACK
+SUBSYS(timer_slack)
+#endif
+
+/* */
--- include/linux/sched.h
+++ include/linux/sched.h
@@ -2855,4 +2855,14 @@ static inline unsigned long rlimit_max(unsigned int limit)
 
 #endif /* __KERNEL__ */
 
+#ifdef CONFIG_CGROUP_TIMER_SLACK
+extern unsigned long task_get_effective_timer_slack(struct task_struct *tsk);
+#else
+static inline unsigned long task_get_effective_timer_slack(
+  		struct task_struct *tsk)
+{
+	return tsk->timer_slack_ns;
+}
+#endif
+
 #endif
