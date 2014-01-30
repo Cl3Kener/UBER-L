48f4176176486319503bb1e58733db99b15e5eab
Chester Kener <Cl3Kener@gmail.com>
2014-01-30 16:41:00 -0500

    Cpufreq: Add Wheatley
    Signed-off-by: Chester Kener <Cl3Kener@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -416,6 +416,9 @@ extern struct cpufreq_governor cpufreq_gov_abyssplugv2;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ONDEMANDPLUS)
 extern struct cpufreq_governor cpufreq_gov_ondemandplus;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_ondemandplus)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_WHEATLEY)
+extern struct cpufreq_governor cpufreq_gov_wheatley;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_wheatley)
 #endif
 
 
