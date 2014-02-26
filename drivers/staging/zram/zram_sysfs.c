d6c852e0f88e06fc668017d68f477d8d43d0436c
Chester Kener <Cl3Kener@gmail.com>
2014-02-26 08:19:36 -0500

    Revert "drivers: staging: Upstream zsmalloc and zram to 3.9"
    This reverts commit 5fb6e3706a7f9952d6817ce22bf6629a06812f6b.

--- drivers/staging/zram/zram_sysfs.c
+++ drivers/staging/zram/zram_sysfs.c
@@ -177,8 +177,10 @@ static ssize_t mem_used_total_show(struct device *dev,
 	u64 val = 0;
 	struct zram *zram = dev_to_zram(dev);
 
-	if (zram->init_done)
-		val = zs_get_total_size_bytes(zram->mem_pool);
+	if (zram->init_done) {
+		val = zs_get_total_size_bytes(zram->mem_pool) +
+			((u64)(zram->stats.pages_expand) << PAGE_SHIFT);
+	}
 
 	return sprintf(buf, "%llu\n", val);
 }
