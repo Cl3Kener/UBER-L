56452a274e731bbfb4e3638c92ee69312c0b59b6
Pranav Vashi <neobuddy89@gmail.com>
2014-03-11 23:07:46 +0530

    fastcharge: New enhancements
    * Add 1800 mA level
    * Add "fake_charge_ac" hack. If fast charge is on with fake_charge_ac = 1,
      device will show current source as AC instead of USB.
    * By default, fake_charge_ac is disabled.
    
    Sincere thanks to mankindtw@xda for idea.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/power/bq24192_charger.c
+++ drivers/power/bq24192_charger.c
@@ -315,8 +315,9 @@ static struct input_ma_limit_entry icl_ma_table[] = {
 	{900, 0x03},
 	{1200, 0x04},
 	{1500, 0x05},
-	{2000, 0x06},
-	{3000, 0x07},
+	{1800, 0x06},
+	{2000, 0x07},
+	{3000, 0x08},
 };
 
 #define INPUT_CURRENT_LIMIT_MIN_MA  100
@@ -367,8 +368,12 @@ static int bq24192_set_input_i_limit(struct bq24192_chip *chip, int ma)
 				i = 5;
 				custom_ma = FAST_CHARGE_1500;
 				break;
-			case FAST_CHARGE_2000:
+			case FAST_CHARGE_1800:
 				i = 6;
+				custom_ma = FAST_CHARGE_1800;
+				break;
+			case FAST_CHARGE_2000:
+				i = 7;
 				custom_ma = FAST_CHARGE_2000;
 				break;
 			default:
