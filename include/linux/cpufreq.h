06e30573cea96fc9c495d2e9d5360a9379f208fd
Chester Kener <Cl3Kener@gmail.com>
2014-03-20 08:14:49 -0400

    cpufreq: Add Nightmare
    Signed-off-by: Chester Kener <Cl3Kener@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -428,6 +428,9 @@ extern struct cpufreq_governor cpufreq_gov_dancedance;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIACTIVE)
 extern struct cpufreq_governor cpufreq_gov_intelliactive;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_intelliactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_NIGHTMARE)
+extern struct cpufreq_governor cpufreq_gov_nightmare;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_nightmare)
 #endif
 
 
