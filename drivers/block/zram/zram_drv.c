e4c6d6d85c555245264ae84a48d27b4f50e66731
Chester Kener <Cl3Kener@gmail.com>
2014-04-08 09:48:05 -0400

    zram: Remove rs_time function completely
    * This function was weighed, measured, and found wanting....
    
    Signed-off-by: Chester Kener <Cl3Kener@gmail.com>
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -31,7 +31,6 @@
 #include <linux/slab.h>
 #include <linux/string.h>
 #include <linux/vmalloc.h>
-#include <linux/ratelimit.h>
 
 #include "zram_drv.h"
 
@@ -39,12 +38,6 @@
 static int zram_major;
 static struct zram *zram_devices;
 static const char *default_compressor = "lzo";
-
-/*
- * We don't need to see memory allocation errors more than once every 1
- * second to know that a problem is occurring.
- */
-#define ALLOC_ERROR_LOG_RATE_MS 1000
 
 /* Module params (documentation at end) */
 static unsigned int num_devices = 1;
@@ -427,10 +420,8 @@ static int zram_bvec_write(struct zram *zram, struct bio_vec *bvec, u32 index,
 
 	handle = zs_malloc(meta->mem_pool, clen);
 	if (!handle) {
-		if (printk_timed_ratelimit(&zram_rs_time,
-					   ALLOC_ERROR_LOG_RATE_MS))
-			pr_info("Error allocating memory for compressed page: %u, size=%zu\n",
-				index, clen);
+		pr_info("Error allocating memory for compressed page: %u, size=%zu\n",
+			index, clen);
 		ret = -ENOMEM;
 		goto out;
 	}
