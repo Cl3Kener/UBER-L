ead1ebe47a9933ea096f7f63bd6e053bf7152a74
Chet Kener <Cl3Kener@gmail.com>
2014-04-22 06:25:59 -0400

    msm_mpdecision: Back off just a tad
    * Currently this is too aggressive just by a tad.  Let's hold this back a bit and give a little bit more space for cpu governors to work their magic before engaging more cores.
    
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- arch/arm/mach-msm/msm_mpdecision.c
+++ arch/arm/mach-msm/msm_mpdecision.c
@@ -98,7 +98,7 @@ static struct msm_mpdec_tuners {
 #endif
 };
 
-static unsigned int NwNs_Threshold[8] = {12, 0, 20, 7, 25, 10, 0, 18};
+static unsigned int NwNs_Threshold[8] = {12, 0, 25, 7, 30, 10, 0, 18};
 static unsigned int TwTs_Threshold[8] = {140, 0, 140, 190, 140, 190, 0, 190};
 
 extern unsigned int get_rq_info(void);
