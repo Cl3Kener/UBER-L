61a1b59b04651cf4aa56be4b60b54811588ae98b
Lars Greiss <kufikugel@googlemail.com>
2014-06-22 00:29:32 +0200

    hammerhead: add slim govenor
    thanks a ton to flar2 where this govenor is based on. Small modifications done
    and more will follow in future.
    
    ToDo: we need to set the govenor as default as well other small modifications
    to ramdisk
    
    Change-Id: Ia7ea6714715103a24e0bbbddcff9e1d0e0771380
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -446,6 +446,9 @@ extern struct cpufreq_governor cpufreq_gov_smartmax;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_OPTIMAX)
 extern struct cpufreq_governor cpufreq_gov_optimax;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_optimax)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SLIM)
+extern struct cpufreq_governor cpufreq_gov_slim;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_slim)
 #endif
 
 
