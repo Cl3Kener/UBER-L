67a8cb3a52a9990fc44692ec17d135e359138f8b
William Clark <wclark@codeaurora.org>
2014-07-17 14:40:41 -0700

    qseecom: fix secure app loading failure by kernel mode qseecom client
    Make a change to fix secure app loading failure due to premature
    kzfree() from __qseecom_load_fw(), when kernel mode qseecom client
    tries to load app.
    
    CRs-fixed: 694541
    Signed-off-by: William Clark <wclark@codeaurora.org>
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- drivers/misc/qseecom.c
+++ drivers/misc/qseecom.c
@@ -1878,7 +1878,6 @@ static int __qseecom_load_fw(struct qseecom_dev_handle *data, char *appname)
 
 	ret = __qseecom_enable_clk_scale_up(data);
 	if (ret) {
-		kzfree(img_data);
 		ret = -EIO;
 		goto loadfw_err;
 	}
@@ -1888,7 +1887,6 @@ static int __qseecom_load_fw(struct qseecom_dev_handle *data, char *appname)
 	ret = scm_call(SCM_SVC_TZSCHEDULER, 1,	&load_req,
 			sizeof(struct qseecom_load_app_ireq),
 			&resp, sizeof(resp));
-	kzfree(img_data);
 	if (ret) {
 		pr_err("scm_call to load failed : ret %d\n", ret);
 		ret = -EIO;
@@ -1916,6 +1914,7 @@ static int __qseecom_load_fw(struct qseecom_dev_handle *data, char *appname)
 	}
 
 loadfw_err:
+	kzfree(img_data);
 	__qseecom_disable_clk_scale_down(data);
 	if (qseecom.support_bus_scaling) {
 		mutex_lock(&qsee_bw_mutex);
