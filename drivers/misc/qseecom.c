93f34f6dd3664feb00c8cc58eccbc73053965b2c
Chet Kener <Cl3Kener@gmail.com>
2014-07-28 08:10:25 -0400

    qseecom: Fix Merge Errors
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- drivers/misc/qseecom.c
+++ drivers/misc/qseecom.c
@@ -72,7 +72,7 @@
 #define RPMB_SERVICE			0x2000
 
 #define QSEECOM_SEND_CMD_CRYPTO_TIMEOUT	2000
-#define QSEECOM_LOAD_APP_CRYPTO_TIMEOUT	4000
+#define QSEECOM_LOAD_APP_CRYPTO_TIMEOUT	2000
 
 enum qseecom_clk_definitions {
 	CLK_DFAB = 0,
@@ -4197,6 +4197,7 @@ static int __devinit qseecom_probe(struct platform_device *pdev)
 
 	/* register client for bus scaling */
 	if (pdev->dev.of_node) {
+		qseecom.pdev->of_node = pdev->dev.of_node;
 		qseecom.support_bus_scaling =
 				of_property_read_bool((&pdev->dev)->of_node,
 						"qcom,support-bus-scaling");
@@ -4226,7 +4227,7 @@ static int __devinit qseecom_probe(struct platform_device *pdev)
 		if (qseecom.support_pfe) {
 			if (of_property_read_u32((&pdev->dev)->of_node,
 				"qcom,file-encrypt-pipe-pair",
-				&qseecom.ce_info.disk_encrypt_pipe)) {
+				&qseecom.ce_info.file_encrypt_pipe)) {
 				pr_err("Fail to get PFE pipe information.\n");
 				rc = -EINVAL;
 				goto exit_destroy_ion_client;
