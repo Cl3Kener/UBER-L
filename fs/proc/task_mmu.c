db6885703a04099feeefb843acde04d1c57ec7ac
Chet Kener <Cl3Kener@gmail.com>
2014-10-21 15:10:41 -0400

    Revert "seq_file: remove "%n" usage from seq_file users"
    This reverts commit 7c08c0c7291b6384d0f0ec51a7d08efadac67b69.
    
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -82,6 +82,14 @@ unsigned long task_statm(struct mm_struct *mm,
 	return mm->total_vm;
 }
 
+static void pad_len_spaces(struct seq_file *m, int len)
+{
+	len = 25 + sizeof(void*) * 6 - len;
+	if (len < 1)
+		len = 1;
+	seq_printf(m, "%*c", len, ' ');
+}
+
 static void seq_print_vma_name(struct seq_file *m, struct vm_area_struct *vma)
 {
 	const char __user *name = vma_get_anon_name(vma);
@@ -263,6 +271,7 @@ show_map_vma(struct seq_file *m, struct vm_area_struct *vma, int is_pid)
 	unsigned long long pgoff = 0;
 	unsigned long start, end;
 	dev_t dev = 0;
+	int len;
 	const char *name = NULL;
 
 	if (file) {
@@ -280,8 +289,7 @@ show_map_vma(struct seq_file *m, struct vm_area_struct *vma, int is_pid)
 	if (stack_guard_page_end(vma, end))
 		end -= PAGE_SIZE;
 
-	seq_setwidth(m, 25 + sizeof(void *) * 6 - 1);
-	seq_printf(m, "%08lx-%08lx %c%c%c%c %08llx %02x:%02x %lu ",
+	seq_printf(m, "%08lx-%08lx %c%c%c%c %08llx %02x:%02x %lu %n",
 			start,
 			end,
 			flags & VM_READ ? 'r' : '-',
@@ -289,14 +297,14 @@ show_map_vma(struct seq_file *m, struct vm_area_struct *vma, int is_pid)
 			flags & VM_EXEC ? 'x' : '-',
 			flags & VM_MAYSHARE ? 's' : 'p',
 			pgoff,
-			MAJOR(dev), MINOR(dev), ino);
+			MAJOR(dev), MINOR(dev), ino, &len);
 
 	/*
 	 * Print the dentry name for named mappings, and a
 	 * special [heap] marker for the heap:
 	 */
 	if (file) {
-		seq_pad(m, ' ');
+		pad_len_spaces(m, len);
 		seq_path(m, &file->f_path, "\n");
 		goto done;
 	}
@@ -328,21 +336,21 @@ show_map_vma(struct seq_file *m, struct vm_area_struct *vma, int is_pid)
 				name = "[stack]";
 			} else {
 				/* Thread stack in /proc/PID/maps */
-				seq_pad(m, ' ');
+				pad_len_spaces(m, len);
 				seq_printf(m, "[stack:%d]", tid);
 			}
 			goto done;
 		}
 
 		if (vma_get_anon_name(vma)) {
-			seq_pad(m, ' ');
+			pad_len_spaces(m, len);
 			seq_print_vma_name(m, vma);
 		}
 	}
 
 done:
 	if (name) {
-		seq_pad(m, ' ');
+		pad_len_spaces(m, len);
 		seq_puts(m, name);
 	}
 	seq_putc(m, '\n');
