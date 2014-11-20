63f22ed143bfdc1eee7dcbb0405f83c10f97745a
Chet Kener <Cl3Kener@gmail.com>
2014-11-20 09:27:33 -0500

    Revert "android/lowmemorykiller: Account for total_swapcache_pages"
    This reverts commit 9af2b5a22e47bcec5cfb3a7b32e3bf941ba6bef5.
    
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -43,7 +43,6 @@
 #include <linux/delay.h>
 #include <linux/swap.h>
 #include <linux/powersuspend.h>
-#include <linux/fs.h>
 
 #ifdef CONFIG_HIGHMEM
 #define _ZONE ZONE_HIGHMEM
@@ -303,14 +302,8 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 	}
 
 	other_free = global_page_state(NR_FREE_PAGES);
-
-	if (global_page_state(NR_SHMEM) + total_swapcache_pages <
-		global_page_state(NR_FILE_PAGES))
-		other_file = global_page_state(NR_FILE_PAGES) -
-						global_page_state(NR_SHMEM) -
-						total_swapcache_pages;
-	else
-		other_file = 0;
+	other_file = global_page_state(NR_FILE_PAGES) -
+						global_page_state(NR_SHMEM);
 
 	tune_lmk_param(&other_free, &other_file, sc);
 
