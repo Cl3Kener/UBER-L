1c4d6cdc70358756945cb8b6dd58bb9f3a97278d
Jaegeuk Kim <jaegeuk@kernel.org>
2015-03-17 17:16:35 -0700

    f2fs: avoid punch_hole overhead when releasing volatile data
    This patch is to avoid some punch_hole overhead when releasing volatile data.
    If volatile data was not written yet, we just can make the first page as zero.
    
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- fs/f2fs/data.c
+++ fs/f2fs/data.c
@@ -1404,6 +1404,8 @@ int do_write_data_page(struct page *page, struct f2fs_io_info *fio)
 		f2fs_update_extent_cache(&dn);
 		trace_f2fs_do_write_data_page(page, OPU);
 		set_inode_flag(F2FS_I(inode), FI_APPEND_WRITE);
+		if (page->index == 0)
+			set_inode_flag(F2FS_I(inode), FI_FIRST_BLOCK_WRITTEN);
 	}
 out_writepage:
 	f2fs_put_dnode(&dn);
